901947c5181370211ab00b3e26c0ba173cb02427
s223153168 <139822807+s223153168@users.noreply.github.com>
2023-09-28 21:12:42 +1000

    Updated credit task

---
+++ AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>About The Weeknd</h1>
+    <p>The Weeknd, whose real name is Abel Tesfaye, is a Canadian singer, songwriter, and record producer. He is known for his unique blend of R&B, pop, and electronic music.</p>
+    <p>With hit songs like "Blinding Lights," "Save Your Tears," and "In the Night," he has captivated audiences worldwide and has become a global music sensation.</p>
+    <p>Contact The Weeknd's team for inquiries or to book tickets for his world tour!</p>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+  margin: 10px 0;
+}
+</style>
---
+++ HomeView.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="weeknd-concert">
+    <!-- 1a. Text Interpolation -->
+    <h1>The Weeknd's World Tour</h1>
+
+    <h2>Concert Details</h2>
+    <!-- 1a. Text Interpolation -->
+    <p>Date: {{ concertDate }}</p>
+    <!-- 1a. Text Interpolation -->
+    <p>Location: {{ concertLocation }}</p>
+
+    <h2>Concert Information</h2>
+    <!-- 1b. Raw HTML [v-html] -->
+    <p v-html="concertDescription"></p>
+
+    <h2>Buy Tickets</h2>
+    <label for="ticketQuantity">Quantity:</label>
+    <!-- 8a. v-model with <input type="number"> -->
+    <input type="number" v-model="ticketQuantity" />
+
+    <!-- 7a. Event Handling: Listening to Events [v-on:click] -->
+    <button @click="buyTickets">Buy Now</button>
+
+    <h2>Featured Songs</h2>
+    <ul>
+      <!-- 6a. v-for with an Object -->
+      <li v-for="song in featuredSongs" :key="song.id">{{ song.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const concertDate = 'October 15, 2023';
+const concertLocation = 'New York City, NY';
+const concertDescription = `
+  <p>Join us for an unforgettable night with The Weeknd on his world tour! Experience the magic of his music live.</p>
+  <p>Get ready to dance and sing along to your favorite songs.</p>
+`;
+const ticketQuantity = ref(1);
+const featuredSongs = ref([
+  { id: 1, title: 'Blinding Lights' },
+  { id: 2, title: 'Save Your Tears' },
+  { id: 3, title: 'In the Night' },
+]);
+
+// 7b. Method Handlers
+const buyTickets = () => {
+  if (ticketQuantity.value > 0) {
+    // Implement ticket purchase logic here
+    alert(`You've purchased ${ticketQuantity.value} tickets for The Weeknd's concert!`);
+    ticketQuantity.value = 1; // Reset ticket quantity
+  }
+};
+</script>
+
+<style scoped>
+.weeknd-concert {
+  font-family: Arial, sans-serif;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  color: #333;
+  margin-top: 10px;
+}
+
+h2 {
+  color: #333;
+  margin-top: 20px;
+}
+
+p {
+  margin: 5px 0;
+}
+
+button {
+  background-color: #f44;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #d00;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+}
+
+label {
+  display: block;
+  margin-top: 10px;
+  font-weight: bold;
+}
+
+input[type="number"] {
+  width: 50px;
+  padding: 5px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+</style>
